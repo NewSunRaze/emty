cf13cc4ac004b579b670cbd48424c4d8bcc4fa5a
NewSunRaze <andrik918@mail.ru>
2021-03-22 14:44:20 +0400

    com  1.0

---
+++ server.js
@@ -0,0 +1,18 @@
+const { Nuxt, Builder } = require("nuxt");
+const app = require("express")();
+
+const isProd = process.env.NODE_ENV === "production";
+const config = require("./nuxt.config.js");
+
+config.dev = !isProd;
+const nuxt = new Nuxt(config);
+
+if (!isProd) {
+  const builder = new Builder(nuxt);
+  builder.build();
+}
+
+app.use(nuxt.render);
+
+app.listen(3000);
+console.log("Server is listening on localhost:3000");
